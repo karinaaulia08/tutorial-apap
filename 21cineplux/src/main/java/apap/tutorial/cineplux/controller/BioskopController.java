5f4138e439271d56fe348892b5dc2c6cd88d313a
Karina Aulia <kaulia56@gmail.com>
2021-09-22 21:19:48 +0700

    feat: edit update penjaga

--- 21cineplux/src/main/java/apap/tutorial/cineplux/controller/BioskopController.java
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/controller/BioskopController.java
@@ -5,10 +5,14 @@ import apap.tutorial.cineplux.model.PenjagaModel;
 import apap.tutorial.cineplux.service.BioskopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.convert.JodaTimeConverters;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,9 +53,16 @@ public class BioskopController {
     ) {
         BioskopModel bioskop = bioskopService.getBioskopByNoBioskop(noBioskop);
         List<PenjagaModel> listPenjaga = bioskop.getListPenjaga();
-
+        LocalTime waktuBuka = bioskop.getWaktuBuka();
+        LocalTime waktuTutup = bioskop.getWaktuTutup();
+        LocalTime waktuSekarang = LocalTime.now();
+        Boolean isBioskopOpen = true;
+        if(waktuSekarang.isAfter(waktuTutup) && waktuSekarang.isBefore(waktuBuka)) {
+            isBioskopOpen = false;
+        }
         model.addAttribute("bioskop", bioskop);
         model.addAttribute("listPenjaga", listPenjaga);
+        model.addAttribute("isBioskopOpen", isBioskopOpen);
 
         return "view-bioskop";
     }
